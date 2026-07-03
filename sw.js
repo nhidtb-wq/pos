080d60a0b1e7365ee7bc578be6d032f8c0e526b1
nhibich <nhi.dtb@newera.inc>
2026-07-03 11:08:03 +0700

    Bump SW to pos-v3 to force cache refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pos-v2';
+const CACHE = 'pos-v3';
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.delete('pos-v1')); });
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
